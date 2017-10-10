6aa19b1e641403c4ab7793f2e7bffe499a4059bc
Sebastian Franco <cebasfu93@gmail.com>
2017-10-10 11:22:37 -0500

    Update configuration.sh

--- Configuracion/configuration.sh
+++ Configuracion/configuration.sh
@@ -2,5 +2,5 @@ cd
 wget https://raw.githubusercontent.com/cebasfu93/Bio-Computo/master/Configuracion/config
 mv config .ssh/
 echo 'export SCHRODINGER='/opt/schrodinger2017-3/' ' >> .bashrc
-sudo chown -R administrador:aministrador .schrodinger/
+sudo chown -R administrador:administrador .schrodinger/
 
