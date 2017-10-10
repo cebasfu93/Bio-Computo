a54cdcf8e4bb2fe44e333498fd0a352729b2d165
Sebastian Franco <cebasfu93@gmail.com>
2017-10-10 10:45:43 -0500

    configuration bash script

---
+++ Configuracion/configuration.sh
@@ -0,0 +1,6 @@
+cd
+wget https://raw.githubusercontent.com/cebasfu93/Bio-Computo/master/Configuracion/config
+mv config .ssh/
+echo 'export SCHRODINGER='/opt/schrodinger2017-3/' ' >> .bashrc
+sudo chown -R administrador:aministrador .schrodinger/
+
